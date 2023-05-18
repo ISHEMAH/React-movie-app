85e9c45b33f239558067e679e89b6c51076f956f
ISHEMAH <121429065+ISHEMAH@users.noreply.github.com>
2023-05-18 20:36:36 +0200

    movie app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import 'swiper/swiper.min.css'
 import './assets/boxicons-2.1.4/css/boxicons.min.css';
 import './App.scss';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Routes from './Config/Routes';
 function App() {
   return (
-    <div className="App">
-      App
-    </div>
+    <BrowserRouter>
+    <Route render={props => (
+      <>
+        <Header {...props}/>
+        <Routes/>
+        <Footer />
+      </>
+    )}/>
+    </BrowserRouter>
+
   );
 }
 
